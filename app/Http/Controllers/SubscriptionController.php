1b84c53ed7447565107f84c05c77fbf224be44bd
gillesarmand225 <armandndah@outlook.com>
2020-09-18 17:39:30 +0000

    fix bug code customer

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -2,13 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Agency;
+use App\Agent;
 use App\Subscription;
 use App\Customer;
 use App\payments;
+use App\Manager;
+use App\Partner;
+use App\Product;
+use App\Role;
+use App\User;
+use App\Vocabulary;
+use App\VocabularyType;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 use App\Providers\AppServiceProvider;
-use App\transsubscription;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
+use Illuminate\database\Query\Builder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\View\View;
+
+use function GuzzleHttp\Promise\all;
 
 class SubscriptionController extends Controller
 {
@@ -22,9 +37,43 @@ class SubscriptionController extends Controller
     public function getcustomers(Request $request)
     {
         //
-        $Subscription = $request->session()->get('Subscription');
 
-        return view('pages.Customers',compact('Subscription'));
+        //$hsubscriptions= Subscription::all();
+        $hsubscriptions = DB::table('customers')
+        ->join('subscriptions', 'subscriptions.customer_id', '=', 'customers.id')
+        ->select('*')
+        ->get();
+
+        $code = Agency::Where("id",Agent::where("username","=",Auth()->user()->username)->first()->agency_id)->first()->partner_id;
+        $codepart=Partner::where("id",$code)->first()->code;
+        $date= date_format(date_create(now()),'d-m-Y');
+        list($jour,$mois,$annee)=sscanf($date,"%d-%d-%d");
+        if (strlen($mois)===1 ) {
+            $mois ='0'.$mois;
+        }else {
+            $mois =$mois;
+        }
+        if (strlen($jour)===1){
+            $jour ='0'.$jour;
+        }else {
+            $jour =$jour;
+        }
+
+        $madate=$jour.$mois.$annee;
+        $dat= date_format(date_create(now()),'Y-m-d');
+        $yest = now()->addDay(-1);
+        $tmr = now()->addDay(1);
+        $partner = Agent::where("username","=",Auth()->user()->username)->first()->agency->partner;
+        $subscription = Subscription::all()->whereIn('agent_id',Agent::all()->whereIn('agency_id',Agency::all()->where('partner_id', '=',$partner->id)->pluck('id'))->pluck('id'))->whereBetween('date_subscription',[$yest,$tmr])->count();
+        $numdossier =$codepart.$madate.str_pad($subscription+1, 5, "0", STR_PAD_LEFT);
+
+        $Subscription = new \App\Subscription();
+        $Subscription->fill(['folder' =>$numdossier]);
+
+        $request->session()->put('Subscription', $Subscription);
+
+       // dd($Subscription);
+        return view('pages.customers',compact('Subscription','numdossier'))->with('hsubscription',$hsubscriptions) ;
 
     }
 
@@ -41,62 +90,14 @@ class SubscriptionController extends Controller
              'marital_status'=> 'required:customers',
              'place_birth'=> 'required:customers',
              'place_residence'=> 'required:customers',
-             'phone1'=> 'required:customers',
-             'phone2'=> 'required:customers',
-             'mail'=> 'required:customers',
+             'phone1'=> 'required|unique:customers',
+             'phone2'=> ':customers',
+             'mail'=> ':customers',
+             'folder'=> ':customers',
+             'mailing_address'=> ':customers',
 
         ]);
  //var_dump($validatedData);exit();
- if(empty($request->session()->get('Subscription'))){
-    $Subscription = new \App\Subscription();
-    $Subscription->fill($validatedData);
-
-    $request->session()->put('Subscription', $Subscription);
-}else{
-    $Subscription = $request->session()->get('Subscription');
-    $Subscription->fill($validatedData);
-
-    $request->session()->put('Subscription', $Subscription);
-}
-       return redirect(route('subscription.getequipment'));
-    }
-
-
-    public function index(Request $request)
-    {
-        //
-        $Subscription = $request->session()->get('Subscription');
-
-        return view('pages.Subscriptions',compact('Subscription'));
-
-    }
-
-    public function getequipment(Request $request)
-    {
-        //
-        $Subscription = $request->session()->get('Subscription');
-
-        return view('pages.Subscriptions',compact('Subscription'));
-
-    }
-
-
-    public function postequipment(Request $request)
-    {
-        //
-        $validatedData = $request->validate([
-            'code'=> ':subscriptions',
-            'equipment'=> 'required:subscriptions',
-            'model'=> 'required:subscriptions',
-            'mark'=> 'required:subscriptions',
-            'numberIMEI'=> 'required:subscriptions',
-            'picture'=> 'required:subscriptions',
-            'price'=> 'required:subscriptions',
-             'date_subscription'=> 'required:subscriptions',
-             'customer_id'=> ':subscriptions',
-
-        ]);
-       //var_dump($validatedData);exit();
         if(empty($request->session()->get('Subscription'))){
             $Subscription = new \App\Subscription();
             $Subscription->fill($validatedData);
@@ -108,9 +109,126 @@ class SubscriptionController extends Controller
 
             $request->session()->put('Subscription', $Subscription);
         }
+        //dd($Subscription);
+            return redirect(route('subscription.getequipment'));
+}
 
+
+    public function index(Request $request)
+    {
+        //
+        $Subscription = $request->session()->get('Subscription');
+
+        return view('pages.subscriptions',compact('Subscription'));
+
+    }
+
+    public function getequipment(Request $request)
+    {
+        //
+        $categories = Vocabulary::all()->where("type_id",VocabularyType::where("code","PDT-TYP")->first()->id);
+        $types = Vocabulary::all()->where("type_id",VocabularyType::where("code","PDT-KIND")->first()->id);
+        $labels = Vocabulary::all()->where("type_id",VocabularyType::where("code","PDT-LBL")->first()->id);
+        $models = Vocabulary::all()->where("type_id",VocabularyType::where("code","PDT-MDL")->first()->id);
+
+        $Subscription = $request->session()->get('Subscription');
+
+        return view('pages.subscriptions',compact("Subscription","categories","types","labels","models"));
+
+    }
+
+
+    public function postequipment(Request $request)
+    {
+
+        $equipmentLibelle = Vocabulary::where("type_id",VocabularyType::where("code","PDT-TYP")->first()->id)->find($request->equipment);
+        $equipmentLibelle=$equipmentLibelle['label'];
+
+
+        $marquelibelle = Vocabulary::where("type_id",VocabularyType::where("code","PDT-LBL")->first()->id)->find($request->mark);
+        $marquelibelle=$marquelibelle['label'];
+
+        $modellibelle = Vocabulary::where("type_id",VocabularyType::where("code","PDT-MDL")->first()->id)->find($request->model);
+        $modellibelle=$modellibelle['label'];
+
+        $agent_id = Agent::where("username","=",Auth()->user()->username)->first()->id;
+        $libellepdv = Agency::Where("id",Agent::where("username","=",Auth()->user()->username)->first()->agency_id)->first()->label;
+        $pdv_id = Agency::Where("label",$libellepdv)->first()->id;
+        $date_subscription= date_format(date_create(now()),'Y-m-d');
+//dd($codepdv);
+        $validatedData = $request->validate([
+                'code'=> ':subscriptions',
+                'equipment'=> 'required:subscriptions',
+                'model'=> 'required:subscriptions',
+                'mark'=> 'required:subscriptions',
+                'numberIMEI'=> 'required|unique:subscriptions',
+                'picture'=> ':subscriptions',
+                'price'=> 'required:subscriptions',
+                'premium'=> ':subscriptions',
+                'date_subscription'=> ':subscriptions',
+                'subscription_enddate'=> ':subscriptions',
+                'agent_id'=> ':subscriptions',
+                'customer_id'=> ':subscriptions',
+
+        ]);
+
+        if(empty($request->session()->get('Subscription'))){
+            $Subscription = new \App\Subscription();
+            $Subscription->fill($validatedData);
+            $Subscription->fill([
+                'modellibelle' =>$modellibelle,
+                'marquelibelle' =>$marquelibelle,
+                'equipmentLibelle' =>$equipmentLibelle,
+                'libellepdv' =>$libellepdv,
+                'pdv_id' =>$pdv_id,
+                'agent_id' =>$agent_id,
+                'date_subscription' =>$date_subscription,
+                ]);
+
+            $request->session()->put('Subscription',$Subscription);
+
+        }else{
+            $Subscription = $request->session()->get('Subscription');
+            $Subscription->fill($validatedData);
+            $Subscription->fill([
+                'modellibelle' =>$modellibelle,
+                'marquelibelle' =>$marquelibelle,
+                'equipmentLibelle' =>$equipmentLibelle,
+                'libellepdv' =>$libellepdv,
+                'agent_id' =>$agent_id,
+                'pdv_id' =>$pdv_id,
+                'date_subscription' =>$date_subscription,
+                ]);
+
+            $request->session()->put('Subscription', $Subscription)  ;
+
+        }
+
+        $madate= $Subscription['date_subscription'];
+        $premium = $Subscription['price']*0.10;
+        list($annee,$mois,$jour)=sscanf($madate,"%d-%d-%d");
+        $annee+=1;
+
+        if (strlen($mois)===1) {
+            $mois ='0'.$mois;
+        }else {
+            $mois =$mois;
+        }
+        if (strlen($jour)===1){
+            $jour ='0'.$jour;
+        }else {
+            $jour =$jour;
+        }
+        $subscription_enddate=$annee.'-'.$mois.'-'.$jour;
+
+        $Subscription->fill([
+            'subscription_enddate' =>$subscription_enddate,
+            'premium' =>$premium,
+            ]);
+
+        $request->session()->put('Subscription', $Subscription)  ;
     //proforma document creation
-       // dd($Subscription);
+
 
         $pdf =  App::make('dompdf.wrapper');
 
@@ -148,19 +266,20 @@ class SubscriptionController extends Controller
 
     public function storecustomers(Request $request)
     {
-        $Subscription = $request->session()->get('Subscription');
-//var_dump($Subscription);exit();
-$code =$Subscription['first_name'];
-$digits =strtoupper(substr($code,0, 3));
 
-$ma=1;
-$ma += Customer::max('id');
-$customers_id=$ma;
-$digits =$ma.''.$digits;
-$codeok =str_pad($digits, 10, "0", STR_PAD_BOTH);
+        $Subscription = $request->session()->get('Subscription');
+        //dd($Subscription);
+       /*  $code =$Subscription['first_name'];
+        $digits =strtoupper(substr($code,0, 3)); */
+
+        $ma=1;
+        $ma += Customer::max('id');
+        $customers_id=$ma;
+       /*  $digits =$ma.''.$digits;
+        $codeok =str_pad($digits, 10, "0", STR_PAD_BOTH); */
 //var_dump($codeok);exit();
         Customer::create([
-            'code' =>$codeok,
+            'code' =>$Subscription['folder'],
             'name' =>$Subscription['name'],
            'first_name' =>$Subscription['first_name'],
            'birth_date' =>$Subscription['birth_date'],
@@ -171,20 +290,26 @@ $codeok =str_pad($digits, 10, "0", STR_PAD_BOTH);
            'phone1' =>$Subscription['phone1'],
            'phone2' =>$Subscription['phone2'],
            'mail' =>$Subscription['mail'],
+           'folder' =>$Subscription['folder'],
+           'mailing_address' =>$Subscription['mailing_address'],
 
         ]);
 
         Subscription::create([
-            'code'=>$codeok,
+            'code'=>$Subscription['folder'],
             'equipment' =>$Subscription['equipment'],
             'model' =>$Subscription['model'],
             'mark' =>$Subscription['mark'],
             'picture' =>$Subscription['picture'],
             'numberIMEI' =>$Subscription['numberIMEI'],
             'price' =>$Subscription['price'],
+            'premium' =>$Subscription['premium'],
             'date_subscription' =>$Subscription['date_subscription'],
+            'subscription_enddate' =>$Subscription['subscription_enddate'],
             'customer_id'=>$customers_id,
+            'agent_id' =>$Subscription['agent_id'],
         ]);
+
 
 
         $pdf =  App::make('dompdf.wrapper');
@@ -194,7 +319,7 @@ $codeok =str_pad($digits, 10, "0", STR_PAD_BOTH);
         $pdf-> save(storage_path().'/app/public/received/'.$Subscription['first_name'].$Subscription['phone1'].'.pdf');
 
 
-        return redirect(route('subscription.recu'))->with('success', 'Souscription ('.$codeok. ') effectuée avec succès.');
+        return redirect(route('subscription.recu'))->with('success', 'Souscription ('.$Subscription['folder']. ') effectuée avec succès.');
     }
 
 
@@ -206,24 +331,39 @@ $codeok =str_pad($digits, 10, "0", STR_PAD_BOTH);
     }
 
     public function exportToPDF(Request $request){
-        $Subscription = $request->session()->get('Subscription');
-        $pdf =  App::make('dompdf.wrapper');
 
-        $pdf-> loadView("models.document", compact('Subscription'));
+        $equipmentLibelle = Vocabulary::where("type_id",VocabularyType::where("code","PDT-TYP")->first()->id)->find($request->equipment);
+        $equipmentLibelle=$equipmentLibelle['label'];
+
+        $marquelibelle = Vocabulary::where("type_id",VocabularyType::where("code","PDT-LBL")->first()->id)->find($request->mark);
+        $marquelibelle=$marquelibelle['label'];
+        $pdf =  App::make('dompdf.wrapper');
+        $Subscription = $request->session()->get('Subscription');
+
+        $pdf-> loadView("models.document", compact('Subscription','equipmentLibelle','marquelibelle'));
 
         return $pdf->download($Subscription['first_name'].$Subscription['phone1'].'.pdf');
 
     }
 
     public function proforma(Request $request){
+
+
+        $equipmentLibelle = Vocabulary::where("type_id",VocabularyType::where("code","PDT-TYP")->first()->id)->find($request->equipment);
+        $equipmentLibelle=$equipmentLibelle['label'];
+
+        $marquelibelle = Vocabulary::where("type_id",VocabularyType::where("code","PDT-LBL")->first()->id)->find($request->mark);
+        $marquelibelle=$marquelibelle['label'];
         $Subscription = $request->session()->get('Subscription');
+
         $pdf =  App::make('dompdf.wrapper');
 
-        $pdf-> loadView("models.model_souscription_summary", compact('Subscription'));
+        $pdf-> loadView("models.model_souscription_summary", compact('Subscription','equipmentLibelle','marquelibelle'));
 
         return $pdf->download('proforma/'.$Subscription['first_name'].$Subscription['phone1'].'.pdf');
 
     }
+
 
     /**
      * Show the form for creating a new resource.
@@ -260,10 +400,10 @@ $codeok =str_pad($digits, 10, "0", STR_PAD_BOTH);
      * @param  \App\Subscription  $subscription
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show()
     {
         //
-        $subscription = Subscription::find($id);
+
 
         //return
     }
