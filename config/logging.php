f5d3562d16af6b6cd2ea783e036c7db3ce4ffd5d
gillesarmand225 <armandndah@outlook.com>
2020-09-29 00:59:58 +0000

    fix bug log

--- config/logging.php
+++ config/logging.php
@@ -37,7 +37,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single','slack','daily'],
+            'channels' => ['single','daily'],
             'ignore_exceptions' => false,
         ],
 
